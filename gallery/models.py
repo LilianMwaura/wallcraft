086518346dcaf2c051627b53aadd435016863b7f
LilianMwaura <wambuimwaura28@gmail.com>
2022-05-29 18:25:18 +0300

    craete location class

--- gallery/models.py
+++ gallery/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+class Location(models.Model):
+    location = models.CharField(max_length=60)
+    
+    def __str__(self):
+        """
+    A method that returns a location as a string
+     """
+        return self.location 
+    
+    def save_location(self):
+        """
+    A method that saves a location
+     """
+        self.save()
+    
+    def delete_location(self):
+        """
+    A method that deletes a location
+     """
+        self.delete()
+    
+    class Meta:
+        ordering = ['location']
--- gallery/views.py
+++ gallery/views.py
@@ -3,5 +3,12 @@ from django.http  import HttpResponse
 
 # Create your views here.
 def index(request):
+    
     return render (request,"index.html")
+
+def wall(request):
+    images = Image.objects.all()
+    location = Location.objects.all()
+    return render(request, 'wall.html',{"images":images, "location":location})    
+
 
