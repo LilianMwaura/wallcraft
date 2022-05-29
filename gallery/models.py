4b19f0ccfeaa553aa4d98b69cb0a6beb3546dc02
LilianMwaura <wambuimwaura28@gmail.com>
2022-05-29 18:29:21 +0300

    craete categories class

--- gallery/models.py
+++ gallery/models.py
@@ -24,3 +24,29 @@ class Location(models.Model):
     
     class Meta:
         ordering = ['location']
+
+class categories(models.Model):
+    category = models.CharField(max_length=60)
+     
+    def __str__(self):
+        """
+    A method that returns a category as a string
+     """
+        return self.category    
+    
+    def save_category(self):
+        """
+    A method that saves a category
+     """
+        self.save()
+    
+    def delete_category(self):
+        """
+    A method that deletes a category
+     """
+        self.delete()
+    
+    class Meta:
+        ordering = ['category']  
+
+  
