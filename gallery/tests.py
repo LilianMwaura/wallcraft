bc3c47bb4e0f1aa1e2477afecbc0be0e812a0788
LilianMwaura <wambuimwaura28@gmail.com>
2022-05-29 19:03:05 +0300

    craete categories class

--- gallery/tests.py
+++ gallery/tests.py
@@ -1,3 +1,31 @@
 from django.test import TestCase
-
+from .models import *
 # Create your tests here.
+class LocationTestClass(TestCase):       
+      # Set up method
+    def setUp(self):
+        self.Nairobi= Location(location='Nairobi')
+        
+    def test_instance(self):
+        self.assertTrue(isinstance(self.Nairobi,Location))    
+        
+    def test_save_method(self):
+        self.Nairobi.save_location()
+        locations = Location.objects.all()
+        self.assertTrue(len(locations)>0)  
+class categoriesTestClass(TestCase):       
+      # Set up method
+    def setUp(self):
+        self.Portrait= categories(category='Portrait')
+        
+    def test_instance(self):
+        self.assertTrue(isinstance(self.Portrait,categories))    
+        
+    def test_save_method(self):
+        self.Portrait.save_category()
+        categorie = categories.objects.all()
+        self.assertTrue(len(categorie)>0)    
+
+
+    
+     
