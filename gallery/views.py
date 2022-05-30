51e26a0bf2628a634bc27733ed408050f480df64
LilianMwaura <wambuimwaura28@gmail.com>
2022-05-30 19:56:38 +0300

    update views.py

--- gallery/views.py
+++ gallery/views.py
@@ -1,14 +1,38 @@
 from django.shortcuts import render
 from django.http  import HttpResponse
+from django.core.exceptions import ObjectDoesNotExist
+from django.http import Http404
+from .models import Image, Location
 
 # Create your views here.
 def index(request):
     
     return render (request,"index.html")
 
-def wall(request):
+def display(request):
     images = Image.objects.all()
     location = Location.objects.all()
-    return render(request, 'wall.html',{"images":images, "location":location})    
+    return render(request, 'display.html',{"images":images, "location":location})
 
+
+def search_results(request):
+    if 'image' in request.GET and request.GET["image"]:
+        search_term = request.GET.get("image")
+        searched_images = Image.search_image(search_term)
+        message = f"{search_term}"
+
+        return render(request,'search.html',{"message":message,"images":searched_images})
+
+    else:
+        message="You haven't searched for any term."
+        return render(request,'search.html',{"message":message})
+    
+def location_results(request, location):
+    
+    try:
+        image_location = Image.filter_by_location(location)
+        message = location
+    except ObjectDoesNotExist:
+        raise Http404()
+    return render(request, 'location.html', {"location": image_location, 'message': location})    
 
