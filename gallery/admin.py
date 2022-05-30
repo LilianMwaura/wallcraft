f54da70aa66eb7857de48295e1df43203c9fb0a3
LilianMwaura <wambuimwaura28@gmail.com>
2022-05-30 21:36:19 +0300

    update display.html

--- gallery/admin.py
+++ gallery/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from .models import *
 # Register your models here.
+class ImageAdmin(admin.ModelAdmin):
+    filter_horizontal =('categories',)
+    
 admin.site.register(Location)
 admin.site.register(categories)
 admin.site.register(Image)
--- gallery/views.py
+++ gallery/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from django.http  import HttpResponse
+from .models import Image, Location
 from django.core.exceptions import ObjectDoesNotExist
 from django.http import Http404
-from .models import Image, Location
+
 
 # Create your views here.
 def index(request):
