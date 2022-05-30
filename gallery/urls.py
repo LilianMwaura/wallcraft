1e2fb5229ec71a5b2ffdc52cf5d4e6f463195cad
LilianMwaura <wambuimwaura28@gmail.com>
2022-05-30 20:55:09 +0300

    update database

--- gallery/urls.py
+++ gallery/urls.py
@@ -5,7 +5,11 @@ from . import views
 
 urlpatterns=[
     path('',views.index, name ='index'),
+    path('display/',views.display, name='display'),
+    path('search/',views.search_results, name='search_results'),
+    path('location/<location>/',views.location_results, name='location_results')
 ]
+
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
     
