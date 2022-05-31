7dcfc06cd0915fbf5cd2c4081ae3e3c8840296bf
LilianMwaura <wambuimwaura28@gmail.com>
2022-05-31 17:56:10 +0300

    deployment

--- wallcraft/settings.py
+++ wallcraft/settings.py
@@ -138,5 +138,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+CSRF_TRUSTED_ORIGINS = ['https://wallcraft01.herokuapp.com']
 import django_heroku
 django_heroku.settings(locals())
